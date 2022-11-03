9d3e37d041bd6cd7f243dc4b7511497e36ce2f50
brennan-lee <brennan-lee@users.noreply.github.com>
2022-11-03 16:14:31 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -4,17 +4,18 @@
 // Created on: nov 2022
 // This file contains the JS functions for index.html
 
-'use strict'
+"use strict"
 /**
  * This function calculates volume of sphere.
  */
-function calculate () {
+function calculate() {
   // input
   const radius = parseFloat(document.getElementById("radius-of-sphere").value)
 
   // process
-  const volume = 4 / 3 * Math.PI * radius ** 3
+  const volume = (4 / 3) * Math.PI * radius ** 3
 
   // output
-  document.getElementById('volume').innerHTML = 'volume is: ' + volume.toFixed(2) + ' mm³'
+  document.getElementById("volume").innerHTML =
+    "volume is: " + volume.toFixed(2) + " mm³"
 }
